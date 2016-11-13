3c10e26ccdd7b8301c9d8652ac0639f665ef35ed
Justin Collins <justin@justincollins.com>
2016-11-13 15:27:57 -0700

    added firebase dependency updated versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+	title = 'app works!';
+	items: FirebaseListObservable<any[]>;
+
+	constructor(af: AngularFire) {
+		this.items = af.database.list('/items');
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
 import { RouterModule }   from '@angular/router';
 
+export const firebaseConfig = {
+	apiKey: 'AIzaSyAzTB1c6OrjzK1e5ZcGgtb8AAtZqcrGFRE',
+	authDomain: 'cataloger-6dc97.firebaseapp.com',
+	databaseURL: 'https://cataloger-6dc97.firebaseio.com',
+	storageBucket: 'cataloger-6dc97.appspot.com',
+	messagingSenderId: '895381901707'
+};
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    CategoryComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule,
-    CatalogerRoutingModule,
-    MaterialModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CategoryComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		RouterModule,
+		CatalogerRoutingModule,
+		MaterialModule.forRoot(),
+		AngularFireModule.initializeApp(firebaseConfig)
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
